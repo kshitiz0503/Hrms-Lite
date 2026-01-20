37644cfa87ca65a4a463975dc3ed489d61b3deb7
Kshitiz Mehta <kshitumehta0521@gmail.com>
2026-01-20 17:30:35 +0530

    requiremets.txct file chnage

--- backend/hrms_backend/settings.py
+++ backend/hrms_backend/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-!to7gqd39=di_sl*mn#h0tmwt!gz@o86zwp0ow+me(comc97!7
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['.onrender.com', 'localhost', '127.0.0.1']
 
 
 SESSION_COOKIE_HTTPONLY = True
@@ -70,6 +70,7 @@ INSTALLED_APPS = [
 MIDDLEWARE = [
     'corsheaders.middleware.CorsMiddleware',
     'django.middleware.security.SecurityMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
@@ -149,6 +150,8 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.2/howto/static-files/
 
 STATIC_URL = 'static/'
+STATIC_ROOT = BASE_DIR / 'staticfiles'
+
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
